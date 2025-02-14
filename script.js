d1a5330bc8a21a26ba2b5f21ec180d64f8d7991c
KARTIK GUPTA <77265018+kartikgupta1920@users.noreply.github.com>
2025-02-15 04:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    if (arr.length === 1) return 0; // If only one rope, no cost is needed.
+
+    const minHeap = new MinPriorityQueue({ priority: x => x });
+
+    // Add all elements to the min heap
+    arr.forEach(num => minHeap.enqueue(num));
+
+    let totalCost = 0;
+
+    // Merge ropes until only one remains
+    while (minHeap.size() > 1) {
+        let first = minHeap.dequeue().element;
+        let second = minHeap.dequeue().element;
+        let cost = first + second;
+        totalCost += cost;
+        minHeap.enqueue(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
+
